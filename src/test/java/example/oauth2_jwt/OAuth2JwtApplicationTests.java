a42f17a76db6a8d9ed8bea88da884fcf6ef3a27d
winy3ori <winy3ori@gmail.com>
2024-11-18 16:22:10 +0900

    first

---
+++ src/test/java/example/oauth2_jwt/OAuth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package example.oauth2_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
