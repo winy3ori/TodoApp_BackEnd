afb9090118c780cf5fbc2bca94ea12c44fae24e2
winy3ori <winy3ori@gmail.com>
2024-11-18 17:02:17 +0900

    유저 정보 DB 저장

---
+++ src/main/java/example/oauth2_jwt/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package example.oauth2_jwt.repository;
+
+import example.oauth2_jwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    UserEntity findByUsername(String username);
+}
--- src/main/java/example/oauth2_jwt/service/CustomOAuth2UserService.java
+++ src/main/java/example/oauth2_jwt/service/CustomOAuth2UserService.java
@@ -1,38 +1,53 @@
 package example.oauth2_jwt.service;
 
 import example.oauth2_jwt.dto.*;
+import example.oauth2_jwt.entity.UserEntity;
+import example.oauth2_jwt.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-    @Service
-    public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+@Service
+@AllArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
-        @Override
-        public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+    private final UserRepository userRepository;
 
-            OAuth2User oAuth2User = super.loadUser(userRequest);
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
-            System.out.println(oAuth2User);
+        OAuth2User oAuth2User = super.loadUser(userRequest);
 
-            String registrationId = userRequest.getClientRegistration().getRegistrationId();
-            OAuth2Response oAuth2Response = null;
-            if (registrationId.equals("naver")) {
+        System.out.println(oAuth2User);
 
-                oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
-            }
-            else if (registrationId.equals("google")) {
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2Response oAuth2Response = null;
+        if (registrationId.equals("naver")) {
 
-                oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
-            }
-            else {
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+        } else if (registrationId.equals("google")) {
 
-                return null;
-            }
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
+        } else {
 
-            String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+            return null;
+        }
+
+        String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+        UserEntity existData = userRepository.findByUsername(username);
+
+        if (existData == null) {    // 처음 로그인 한 경우
+
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setRole("ROLE_USER");
+
+            userRepository.save(userEntity);
 
             UserDTO userDTO = new UserDTO();
             userDTO.setUsername(username);
@@ -40,6 +55,20 @@ import org.springframework.stereotype.Service;
             userDTO.setRole("ROLE_USER");
 
             return new CustomOAuth2User(userDTO);
+        } else {  // 한 번 이라도 로그인 한 적 있는 경우
+
+            existData.setEmail(oAuth2Response.getEmail());
+            existData.setName(oAuth2Response.getName());
+
+            userRepository.save(existData);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(existData.getUsername());
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(userDTO);
 
         }
     }
+}
